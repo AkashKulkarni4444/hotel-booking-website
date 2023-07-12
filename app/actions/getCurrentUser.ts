309d2e6ea873f76125b25a03380e4ba8bf58bd90
Akash <akashkulkarni4444@gmail.com>
2023-07-12 19:55:04 +0530

    Initial commit

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,35 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from "@/app/libs/prismadb";
+import { SafeUser } from "../types";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
+    });
+    if (!currentUser) {
+      return null;
+    }
+    return <SafeUser>{
+      ...currentUser,
+      createdAt: currentUser.createdAt.toISOString(),
+      updatedAt: currentUser.updatedAt.toISOString(),
+      emailVerified: currentUser.emailVerified?.toISOString(),
+    };
+  } catch (error) {
+    return null;
+  }
+}
